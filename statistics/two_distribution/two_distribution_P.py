a44f6d733a6e3cbcb99bdba6ae172fa1df74cd83
Chandler <ysiwgtus@gmail.com>
2018-03-27 12:50:10 +0800

    二项分布

---
+++ statistics/two_distribution/two_distribution_P.py
@@ -0,0 +1,13 @@
+import math
+def distribution(x,n):
+    """
+    :param x: 抛硬币次数
+    :param n: 抛硬币x次得到正面次数n
+    :return: 抛硬币x次得到n次正面的概率
+    """
+    a = (1/2)**x
+    denominator = math.factorial(n)*math.factorial((x-n))
+    Numerator = math.factorial(x)
+    b = Numerator/denominator
+    Probability = a*b
+    return Probability
