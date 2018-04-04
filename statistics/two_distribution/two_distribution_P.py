3ab5934dc61a78206f3c681749397e6e304b1df5
Chandler <ysiwgtus@gmail.com>
2018-04-04 13:03:24 +0800

    Debug

--- statistics/two_distribution/two_distribution_P.py
+++ statistics/two_distribution/two_distribution_P.py
@@ -30,4 +30,22 @@ class Distribution(object):
         return Probability
 
 
+class iter(object):
+    def __init__(self, max):
+        self.max = max
+        self.n = 0
 
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n < self.max:
+            self.n += 1
+            return self.n
+        else:
+            raise StopIteration
+
+
+t = iter(5)
+t.next()
